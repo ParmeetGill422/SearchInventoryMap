1a3562794e0427d84976288cc6846cd9650e6315
Parmeet Gill <parmeetgill422@gmail.com>
2025-04-23 12:39:52 -0700

    cc

--- src/Product.java
+++ src/Product.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Product {
+    private int id;
+    private String name;
+    private double price;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public Product(int id, String name, double price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+    public int getId(){ return id;}
+    public String getName(){ return name;}
+    public double getPrice(){ return price;}
+
+    public String toString(){
+        return String.format("%d | %s | %.2f", id, name, price);
     }
 }
